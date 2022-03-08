f9026e902324bb6d6b55e691287be331f0647740
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-08 21:17:58 +0800

    >  sim RTL ysyx_220088 吴佳宾 Linux bill-virtual-machine 5.13.0-30-generic #33~20.04.1-Ubuntu SMP Mon Feb 7 14:25:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:17:58 up 29 min,  1 user,  load average: 0.07, 0.10, 0.11

---
+++ npc/hdl/mux41b_tb.cpp
@@ -0,0 +1,38 @@
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "obj_dir/Vmux41b.h"
+
+VerilatedContext* contextp = NULL;
+VerilatedVcdC* tfp = NULL;
+
+static Vmux41b* top;
+
+void step_and_dump_wave() {
+    top->eval();
+    contextp->timeInc(1);
+    tfp->dump(contextp->time());
+}
+
+void sim_init() {
+    contextp = new VerilatedContext;
+    tfp = new VerilatedVcdC;
+    top = new Vm_mux21;
+    contextp->traceEverOn(true);
+    top->trace(tfp, 0);
+    tfp->open("dump.vcd");
+}
+
+void sim_exit() {
+    step_and_dump_wave();
+    tfp->close();
+}
+
+int main() {
+	sim_init();
+	top->x0 = 0b00;	top->x1 = 0b01;	top->x2 = 0b10;	top->x3 = 0b11;
+	top->y = 0b00;	top->step_and_dump_wave();
+	top->y = 0b01;	top->step_and_dump_wave();
+	top->y = 0b10;	top->step_and_dump_wave();
+	top->y = 0b11;	top->step_and_dump_wave();
+	sim_exit();
+}
