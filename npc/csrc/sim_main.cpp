1793b0ce347da6978b8f54a98cad7733381d6037
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-08 20:04:43 +0800

    >  sim RTL ysyx_220088 吴佳宾 Linux bill-virtual-machine 5.13.0-30-generic #33~20.04.1-Ubuntu SMP Mon Feb 7 14:25:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:04:43 up  6:25,  1 user,  load average: 1.86, 1.88, 1.57

---
+++ npc/csrc/sim_main.cpp
@@ -0,0 +1,32 @@
+#include <nvboard.h>
+#include <Vtop.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+static TOP_NAME top;
+void nvboard_bind_all_pins(Vtop* top);
+
+void single_cycle() {
+	top.clk = 0; top.eval();
+	top.clk = 1; top.eval();
+}
+
+void reset(int n) {
+	top.rst = 1;
+	while (n-- > 0) single_cycle();
+	top.rst = 0;
+}
+
+int main() {
+	nvboard_bind_all_pins(&top);
+	nvboard_init();
+
+	reset(10); // 复位10个周期
+
+	while (1) {
+		nvboard_update();
+		single_cycle();
+	}
+	return 0;
+}
