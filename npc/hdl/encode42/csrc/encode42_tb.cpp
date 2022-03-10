38743f9282a9a80e1f2caaf11997769ec1ea6e20
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-10 21:54:31 +0800

    >  sim RTL ysyx_220088 吴佳宾 Linux bill-virtual-machine 5.13.0-30-generic #33~20.04.1-Ubuntu SMP Mon Feb 7 14:25:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:54:31 up  3:37,  1 user,  load average: 1.03, 0.98, 0.77

--- npc/hdl/encode42/csrc/encode42_tb.cpp
+++ npc/hdl/encode42/csrc/encode42_tb.cpp
@@ -33,13 +33,13 @@ int main() {
 
   top->en=0b0; top->x =0b0000; step_and_dump_wave();
                top->x =0b0001; step_and_dump_wave();
-               top->x =0b0010; step_and_dump_wave();
-               top->x =0b0100; step_and_dump_wave();
-               top->x =0b1000; step_and_dump_wave();
+               top->x =0b0011; step_and_dump_wave();
+               top->x =0b0111; step_and_dump_wave();
+               top->x =0b1111; step_and_dump_wave();
   top->en=0b1; top->x =0b0000; step_and_dump_wave();
                top->x =0b0001; step_and_dump_wave();
-               top->x =0b0010; step_and_dump_wave();
-               top->x =0b0100; step_and_dump_wave();
-               top->x =0b1000; step_and_dump_wave();
+               top->x =0b0011; step_and_dump_wave();
+               top->x =0b0111; step_and_dump_wave();
+               top->x =0b1111; step_and_dump_wave();
   sim_exit();
 }
--- npc/hdl/encode42/vsrc/encode42.v
+++ npc/hdl/encode42/vsrc/encode42.v
@@ -3,16 +3,13 @@ module encode42(
     input       en,
     output reg [1:0]y
 );
+    integer i;
 
     always @(x or en) begin
         if (en) begin
-            case (x)
-            4'b0001: y = 2'b00;
-            4'b0010: y = 2'b01;
-            4'b0100: y = 2'b10;
-            4'b1000: y = 2'b11;
-            default: y = 2'b00;
-            endcase
+            y = 0;
+            for (i = 0; i <= 3; i = i + 1)
+                if (x[i] == 1)  y = i[1:0]
         end
         else y = 2'b00;
     end
