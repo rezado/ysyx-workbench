676b396e2001b100ee7f79c548ab8c932f7b32ac
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-16 21:40:22 +0800

    >  sim RTL ysyx_220088 吴佳宾 Linux bill-virtual-machine 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:40:22 up  3:14,  1 user,  load average: 0.32, 0.18, 0.25

--- npc/hdl/ex6/barrelshifter/vsrc/top.v
+++ npc/hdl/ex6/barrelshifter/vsrc/top.v
@@ -4,14 +4,10 @@ module top(
     output  reg [7:0] out
 );
 
-    reg hi;
-    reg [7:0] t;
     always @(posedge clk or posedge rst) begin
         if (rst)    out <= 8'b00000001;
         else begin
-        t <= out;
-        hi <= ^t[3:0];
-        out <= {hi, out[7:1]};
+        out <= {^out[3:0], out[7:1]};
         end
     end
 
