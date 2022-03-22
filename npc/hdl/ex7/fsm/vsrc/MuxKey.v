75b437ccd8fa567c51a3dca70c59468336699ba4
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-22 11:40:55 +0800

    >  sim RTL ysyx_220088 吴佳宾 Linux bill-virtual-machine 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:40:55 up  1:11,  1 user,  load average: 0.18, 0.25, 0.23

--- npc/hdl/ex7/fsm/vsrc/MuxKey.v
+++
@@ -1,7 +0,0 @@
-module MuxKey #(NR_KEY = 2, KEY_LEN = 1, DATA_LEN = 1) (
-  output [DATA_LEN-1:0] out,
-  input [KEY_LEN-1:0] key,
-  input [NR_KEY*(KEY_LEN + DATA_LEN)-1:0] lut
-);
-  MuxKeyInternal #(.NR_KEY(NR_KEY), .KEY_LEN(KEY_LEN), .DATA_LEN(DATA_LEN), .HAS_DEFAULT(0)) i0 (out, key, 1'b0, lut);
-endmodule
