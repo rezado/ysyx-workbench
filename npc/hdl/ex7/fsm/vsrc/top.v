0b27f789ecc477c78f054cedbaf8f12a2f46949d
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-22 11:44:06 +0800

    >  sim RTL ysyx_220088 吴佳宾 Linux bill-virtual-machine 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:44:06 up  1:14,  1 user,  load average: 0.30, 0.29, 0.25

--- npc/hdl/ex7/fsm/vsrc/top.v
+++ npc/hdl/ex7/fsm/vsrc/top.v
@@ -7,7 +7,7 @@ module top
 parameter[3:0] S0 = 0, S1 = 1, S2 = 2, S3 = 3,
           S4 = 4, S5 = 5, S6 = 6, S7 = 7, S8 = 8;
 
-wire [3:0] state_din, state_dout;  // dout现态 din次态
+reg [3:0] state_din, state_dout;  // dout现态 din次态
 wire state_wen;
 
 SimReg#(4,0) state(.clk(clk), .reset(reset), .state_wen(state_wen), .state_dout(state_dout), .state_din(state_din));
