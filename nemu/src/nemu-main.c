faefb7ffca5e8ef5c6c0d5c4a75408729e08a260
tracer-ics2021 <tracer@njuics.org>
2022-04-13 10:49:12 +0800

    >  compile 220088    Static hostname: bill-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: e31451999dc64393b0c7f9f04d3aa4c2            Boot ID: 0a550d7b3ebc40f4b5443238d3d13783     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-39-generic       Architecture: x86-64  10:49:13 up  2:50,  1 user,  load average: 1.46, 1.09, 0.71

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[]) {
   uint32_t result;
   FILE *fp = fopen("/home/bill/ysyx-workbench/nemu/src/wrong", "r");
   FILE *output = fopen("/home/bill/ysyx-workbench/nemu/src/output", "w");
-  // FILE *wrong = fopen("/home/bill/ysyx-workbench/nemu/src/wrong", "w");
+  FILE *wrong = fopen("/home/bill/ysyx-workbench/nemu/src/wrong", "w");
   assert(fp != NULL);
   while (fscanf(fp, "%u", &result) != EOF) {
     if (fgets(buf, sizeof buf, fp) == NULL) break;
@@ -32,13 +32,12 @@ int main(int argc, char *argv[]) {
     if (tmp != result) {
         fprintf(output, "Wrong:%s\nexpected:%u but get %u\n", buf, result, tmp);
         // printf("Wrong:%s\nexpected:%u but get %u\n", buf, result, tmp);
-        // fputs(buf, wrong);
-        // fputc('\n', wrong);
+        fprintf(wrong, "%u %s\n", result, buf);
     }
   }
   fclose(fp);
   fclose(output);
-  // fclose(wrong);
+  fclose(wrong);
 
   return is_exit_status_bad();
 }
