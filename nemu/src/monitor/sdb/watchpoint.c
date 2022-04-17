5ad1402597c7612b24fc572711117f4b84531a09
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-17 10:37:55 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:37:55 up 21:54,  1 user,  load average: 1.46, 0.70, 0.39

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -7,17 +7,21 @@ typedef struct watchpoint {
   struct watchpoint *next;
 
   /* TODO: Add more members if necessary */
-
+  char *expr;
+  word_t value;
 } WP;
 
 static WP wp_pool[NR_WP] = {};
 static WP *head = NULL, *free_ = NULL;
+static int wp_num = 0;  // 监视点的数目
 
 void init_wp_pool() {
   int i;
   for (i = 0; i < NR_WP; i ++) {
     wp_pool[i].NO = i;
     wp_pool[i].next = (i == NR_WP - 1 ? NULL : &wp_pool[i + 1]);
+    wp_pool[i].expr = NULL;
+    wp_pool[i].value = 0;
   }
 
   head = NULL;
@@ -26,3 +30,73 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
+void new_wp(char *args) {
+  // 没有空闲监视点了
+  if (!free_)
+    assert(0);
+  
+  WP *p = free_;
+  free_ = free_->next;
+  p->NO = wp_num;
+  printf("%s\n", args);
+  p->expr = (char*)malloc(strlen(args) * 8);
+  strcpy(p->expr, args);
+  p->value = 0;
+  p->next = head;
+  head = p;
+  wp_num++;
+}
+
+void free_wp(int NO) {
+  WP *p = head, *pre = NULL;
+  while (p) {
+    if (p->NO == NO) break;
+    pre = p;
+    p = p->next;
+  }
+  if (!p) {
+    printf("不存在监视点\n");
+  }
+  else if (p == head) {
+    // 头结点就是删除节点
+    head = p->next;
+    p->next = free_;
+    free_ = p;
+    wp_num--;
+  }
+  else {
+    // 头插法
+    pre->next = p->next;
+    p->next = free_;
+    free_ = p;
+    wp_num--;
+  }
+}
+
+// 扫描监视点
+bool scan_wp() {
+  bool flag = true;
+  WP *p = head;
+  while (p) {
+    bool success = true;
+    word_t val = expr(p->expr, &success);
+    if (!success) assert(0);
+
+    if (val != p->value) {
+      printf("Hardware watchpoint %d: %s\n\n", p->NO, p->expr);
+      printf("Old value = %lx\nNew value = %lx\n", p->value, val);
+      p->value = val;
+      flag = false;
+    }
+    p = p->next;
+  }
+  return flag;
+}
+
+void print_wp() {
+  WP *p = head;
+  while (p) {
+    printf("%d\t%s\n", p->NO, p->expr);
+    p = p->next;
+  }
+}
