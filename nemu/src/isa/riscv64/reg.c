65354c858db8969c4282ae9e1e4ae2de16f86268
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-17 10:30:21 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:30:21 up 21:46,  1 user,  load average: 0.33, 0.36, 0.24

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -8,25 +8,9 @@ const char *regs[] = {
   "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
 
-// print all regs
 void isa_reg_display() {
-  int i;
-  for(i = 0; i < 32; i++) {
-	  printf("%d %s:0x%016lx\n", i, regs[i], gpr(i)); 
-  }
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
-  if (strcmp(s, "pc") == 0) {
-    *success = true;
-    
-  }
-  for (int i = 0; i < 32; i++) {
-    if (strcmp(s, regs[i]) == 0) {
-      *success = true;
-      return gpr(i);
-    }
-  }
-  *success = false;
   return 0;
 }
--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -16,58 +16,9 @@ static char *code_format =
 "  return 0; "
 "}";
 
-int buf_ptr;
-int buf_overflow;
-
-word_t choose(word_t n) {
-  return rand() % n;
-}
-
-void gen_num() {
-  // 生成随机数
-  int n = choose(UINT32_MAX);
-
-  // 将随机数转字符串
-  char num[12];
-  int cnt = 0, t = n;
-  while (t) {
-    num[cnt++] = t % 10 + '0';
-    t = t / 10;
-  }
-
-  // 写入缓存区
-  for (int i = cnt - 1; i >= 0; i--)
-    buf[buf_ptr++] = num[i];
-  buf[buf_ptr++] = 'u';
-}
-
-void gen(char c) {
-  buf[buf_ptr++] = c;
-}
-
-void gen_rand_op() {
-  switch (choose(4)) {
-    case 0: buf[buf_ptr++] = '+'; break;
-    case 1: buf[buf_ptr++] = '-'; break;
-    case 2: buf[buf_ptr++] = '*'; break;
-    default: buf[buf_ptr++] = '/'; break;
-  }
-}
-
 static void gen_rand_expr() {
-  if(buf_ptr > 65535) {
-    buf_overflow = 1;
-    return;
-  }
-  switch (choose(4)) {
-    case 0: gen_num(); break;
-    case 1: gen('('); gen_rand_expr(); gen(')'); break;
-    case 2: gen(' ');
-    default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
-  }
-  // puts(buf);
+  buf[0] = '\0';
 }
-
 
 int main(int argc, char *argv[]) {
   int seed = time(0);
@@ -78,17 +29,7 @@ int main(int argc, char *argv[]) {
   }
   int i;
   for (i = 0; i < loop; i ++) {
-    // buf初始化 生成随机表达式 
-    buf[0] = '\0';
-    buf_ptr = 0;
-    buf_overflow = 0;  // 缓冲区越界标志
-
     gen_rand_expr();
-    if (buf_overflow) {
-      // i--;
-      continue;  // 丢弃过长的表达式
-    }
-    buf[buf_ptr++] = 0;  // 表达式结束
 
     sprintf(code_buf, code_format, buf);
 
@@ -97,7 +38,7 @@ int main(int argc, char *argv[]) {
     fputs(code_buf, fp);
     fclose(fp);
 
-    int ret = system("gcc /tmp/.code.c -o /tmp/.expr -Werror");
+    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
     if (ret != 0) continue;
 
     fp = popen("/tmp/.expr", "r");
