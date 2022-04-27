996f1fe4dca5d4fa84ecb7ef42538d3f5b083c2a
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-27 16:44:52 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-41-generic #46~20.04.1-Ubuntu SMP Wed Apr 20 13:16:21 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:44:52 up 16:05,  1 user,  load average: 0.45, 0.36, 0.24

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,47 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  char *pri = out;
+  va_list ap;
+  va_start(ap, fmt);
+  assert(out != NULL && fmt != NULL);
+  size_t i;
+  size_t len = strlen(fmt);
+  int flag = 0, t;
+  char op, c;
+  char *str = NULL;
+  for (i = 0; i < len; i++) {
+    if (fmt[i] == '%') {
+      flag = 1;
+    }
+    else if (!flag) {
+      *out = fmt[i];
+      out++;
+    }
+    else {
+      op = fmt[i];
+      if (op == 'd') {
+        t = va_arg(ap, int);
+        while (t) {
+          c = t % 10 + '0';
+          *out = c;
+          out++;
+          t = t / 10;
+        }
+      }
+      else if (op == 's') {
+        str = va_arg(ap, char*);
+        while (*str != '\0') {
+          *out = *str;
+          out++;
+          str++;
+        }
+      }
+    }
+  }
+  va_end(ap);
+
+  return out - pri;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
