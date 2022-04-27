85bd0cde258a28843524cbe2b56713e3dff6f5a6
rezado <1908819406@qq.com>
2022-04-27 20:58:06 +0800

    feat: pass hello-str

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,61 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  char *pri = out;
+  va_list ap;
+  va_start(ap, fmt);
+  size_t i;
+  size_t len = strlen(fmt);
+  int flag = 0, argDec;
+  char op;
+  char *argStr = NULL;
+  char num[50];
+  int t, cnt;  // 一些临时变量
+  for (i = 0; i < len; i++) {
+    if (fmt[i] == '%') {
+      flag = 1;
+    }
+    else if (!flag) {
+      *out = fmt[i];
+      out++;
+    }
+    else {
+      flag = 0;
+      op = fmt[i];
+      if (op == 'd') {
+        argDec = va_arg(ap, int);
+        if (argDec < 0) {
+          *out++ = '-';
+          argDec = -argDec;
+        }
+        else if (argDec == 0)
+          *out++ = '0';
+
+        t = argDec;
+        cnt = 0;
+        while (t) {
+          num[cnt++] = argDec % 10 + '0';
+          t = t / 10;
+        }
+        
+        while (cnt > 0) {
+          *out++ = num[--cnt];
+        }
+      }
+      else if (op == 's') {
+        argStr = va_arg(ap, char*);
+        while (*argStr != '\0') {
+          *out = *argStr;
+          out++;
+          argStr++;
+        }
+      }
+    }
+  }
+  *out++ = 0;
+  va_end(ap);
+
+  return out - pri;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
