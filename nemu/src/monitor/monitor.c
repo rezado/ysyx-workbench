a49f238e78846888d901e7fced80e0bc0c6542e0
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-29 16:58:01 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-41-generic #46~20.04.1-Ubuntu SMP Wed Apr 20 13:16:21 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:58:01 up 23:54,  1 user,  load average: 0.18, 0.19, 0.19

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -77,7 +77,7 @@ static int parse_args(int argc, char *argv[]) {
         printf("\t-l,--log=FILE           output log to FILE\n");
         printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
         printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
-        printf("\t-e,--elf=FILE           read elf file for ftrace");
+        printf("\t-e,--elf=FILE           read elf file for ftrace\n");
         exit(0);
     }
   }
