cb6887c167d2991775395dec803cbeca6b612928
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-01 21:14:03 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-41-generic #46~20.04.1-Ubuntu SMP Wed Apr 20 13:16:21 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:14:03 up 1 day, 21 min,  1 user,  load average: 0.64, 0.49, 0.29

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -14,8 +14,7 @@ override ARGS += $(ARGS_DIFF)
 
 # Command to execute NEMU
 IMG ?=
-ELF ?=
-NEMU_EXEC := $(BINARY) $(ARGS) $(IMG) $(ELF)
+NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
 
 run-env: $(BINARY) $(DIFF_REF_SO)
 
