882ff4cda91d3aac592560d4902319b72e13eea5
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-05 16:50:36 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-41-generic #46~20.04.1-Ubuntu SMP Wed Apr 20 13:16:21 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:50:36 up  2:05,  1 user,  load average: 1.13, 0.43, 0.24

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -75,7 +75,7 @@ int main() {
   reset(4);
   while (sim_time < MAX_SIM_TIME) {
 	  top->inst = pmem_read(top->pc);
-	  printf("%u", top->inst);
+	  printf("%u\n", top->inst);
       single_cycle();
       sim_time++;
   }
