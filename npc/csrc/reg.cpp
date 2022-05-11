dcebeb6539f3a645b796ea60014b1f63a1178779
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-11 17:36:27 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-41-generic #46~20.04.1-Ubuntu SMP Wed Apr 20 13:16:21 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:36:27 up  2:32,  1 user,  load average: 0.55, 0.66, 0.63

--- npc/csrc/reg.cpp
+++ npc/csrc/reg.cpp
@@ -8,7 +8,7 @@ const char *regs[] = {
 };
 
 // 读取寄存器相关
-extern uint64_t *cpu_gpr;
+uint64_t *cpu_gpr;
 extern "C" void set_gpr_ptr(const svOpenArrayHandle r) {
   cpu_gpr = (uint64_t *)(((VerilatedDpiOpenVar*)r)->datap());
 }
--- npc/include/reg.h
+++ npc/include/reg.h
@@ -1,6 +1,6 @@
 #include <common.h>
 
-uint64_t *cpu_gpr = NULL;
+extern uint64_t *cpu_gpr = NULL;
 extern "C" void set_gpr_ptr(const svOpenArrayHandle r);
 void dump_gpr();
 word_t isa_reg_str2val(const char *s, bool *success);
