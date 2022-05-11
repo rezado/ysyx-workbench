a605d0715129626fdedab370829abdc7bb1b7f26
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-11 17:19:20 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-41-generic #46~20.04.1-Ubuntu SMP Wed Apr 20 13:16:21 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:19:20 up  2:15,  1 user,  load average: 1.70, 0.90, 0.62

--- npc/include/memory/paddr.h
+++ npc/include/memory/paddr.h
@@ -8,7 +8,7 @@ uint8_t* guest_to_host(paddr_t paddr);
 /* convert the host virtual address in NEMU to guest physical address in the guest program */
 paddr_t host_to_guest(uint8_t *haddr);
 
-word_t paddr_read(paddr_t addr, int len);
-void paddr_write(paddr_t addr, int len, word_t data);
+word_t pmem_read(paddr_t addr, int len);
+void pmem_write(paddr_t addr, int len, word_t data);
 
 #endif
