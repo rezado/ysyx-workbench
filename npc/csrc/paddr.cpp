98c0dbb16ef38671f129981bda674346db521996
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-11 17:24:28 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-41-generic #46~20.04.1-Ubuntu SMP Wed Apr 20 13:16:21 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:24:28 up  2:20,  1 user,  load average: 0.64, 0.84, 0.69

--- npc/csrc/paddr.cpp
+++ npc/csrc/paddr.cpp
@@ -1,9 +1,6 @@
 #include <memory/host.h>
 #include <memory/paddr.h>
 
-static uint8_t pmem[10010] PG_ALIGN = {};
-#define CONFIG_MBASE 0x80000000
-
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
--- npc/include/memory/paddr.h
+++ npc/include/memory/paddr.h
@@ -3,6 +3,9 @@
 
 #include <common.h>
 
+static uint8_t pmem[10010] PG_ALIGN = {};
+#define CONFIG_MBASE 0x80000000
+
 /* convert the guest physical address in the guest program to host virtual address in NEMU */
 uint8_t* guest_to_host(paddr_t paddr);
 /* convert the host virtual address in NEMU to guest physical address in the guest program */
