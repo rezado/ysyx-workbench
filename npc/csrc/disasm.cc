cc0a65b6dad3f54c22b52c9a90560dd6a22f8d26
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-14 11:25:12 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-41-generic #46~20.04.1-Ubuntu SMP Wed Apr 20 13:16:21 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:25:12 up 10:13,  1 user,  load average: 0.48, 0.42, 0.26

--- npc/csrc/disasm.cc
+++ npc/csrc/disasm.cc
@@ -73,8 +73,8 @@ extern "C" void init_disasm(const char *triple) {
 }
 
 extern "C" void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
-  puts("1");
   MCInst inst;
+  puts("1");
   llvm::ArrayRef<uint8_t> arr(code, nbyte);
   uint64_t dummy_size = 0;
   gDisassembler->getInstruction(inst, dummy_size, arr, pc, llvm::nulls());
