4dede5a37206712ed179ad421a3f0e7357c3cac8
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-14 10:39:01 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-41-generic #46~20.04.1-Ubuntu SMP Wed Apr 20 13:16:21 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:39:01 up  9:27,  1 user,  load average: 0.46, 0.28, 0.19

--- npc/obj_dir/Vtop.mk
+++ npc/obj_dir/Vtop.mk
@@ -49,6 +49,7 @@ VM_USER_CFLAGS = \
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
 	-lreadline \
+	-lLLVM-12 \
 
 # User .cpp files (from .cpp's on Verilator command line)
 VM_USER_CLASSES = \
