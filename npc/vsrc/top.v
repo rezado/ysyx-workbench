4ac162228da0c5b3957b5d9b9e76a5f603ac9891
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-17 16:03:34 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-41-generic #46~20.04.1-Ubuntu SMP Wed Apr 20 13:16:21 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:03:34 up 17:34,  1 user,  load average: 0.79, 0.48, 0.24

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -7,16 +7,10 @@ module top(
 
 // IFU
 ysyx_22040088_IFU u_ysyx_22040088_IFU(
-<<<<<<< HEAD
-	.clk (clk ),
-	.rst (rst ),
-	.pc  (pc  )
-=======
 	.clk    (clk    ),
 	.rst    (rst    ),
 	.nextpc (nextpc ),
 	.pc     (pc     )
->>>>>>> npc
 );
 
 // 控制信号
@@ -24,33 +18,20 @@ wire [11:0] alu_op;
 // 数据
 wire [63:0] alu_src1, alu_src2;
 wire [63:0] rf_wdata;
-<<<<<<< HEAD
-// IDU
-ysyx_22040088_IDU u_ysyx_22040088_IDU(
-	.clk      (clk      ),
-	// .rst      (rst      ),
-=======
 wire [63:0] nextpc;
 // IDU
 ysyx_22040088_IDU u_ysyx_22040088_IDU(
 	.clk      (clk      ),
 	.pc       (pc       ),
->>>>>>> npc
 	.inst     (inst     ),
 	.rf_wdata (rf_wdata ),
 	.alu_op   (alu_op   ),
 	.alu_src1 (alu_src1 ),
-<<<<<<< HEAD
-	.alu_src2 (alu_src2 )
-);
-
-=======
 	.alu_src2 (alu_src2 ),
 	.nextpc   (nextpc   )
 );
 
 
->>>>>>> npc
 // EXU
 ysyx_22040088_EXU u_ysyx_22040088_EXU(
 	// .clk        (clk        ),
@@ -71,8 +52,6 @@ always @(posedge clk) begin
 		$finish();
 	end
 end
-<<<<<<< HEAD
-=======
 
 // inst
 import "DPI-C" function void get_inst(int inst);
@@ -80,6 +59,5 @@ always@(*) begin
 	get_inst(inst);
 end
 
->>>>>>> npc
 
 endmodule
