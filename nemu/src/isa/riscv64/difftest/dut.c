282a203f16c347e8b73e3fd66f129878348485a9
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-18 10:51:31 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-41-generic #46~20.04.1-Ubuntu SMP Wed Apr 20 13:16:21 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:51:31 up  3:36,  1 user,  load average: 0.09, 0.31, 0.44

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -8,7 +8,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
     if (ref_r->gpr[i] != gpr(i))
       return false;
   }
-  if (ref_r->pc != pc)  return false;
+  if (ref_r->pc != pc + 4)  return false;
   return true;
 }
 
