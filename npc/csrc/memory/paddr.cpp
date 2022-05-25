d33fad60313b5e2ebb9a41abcd44a2928b82a1da
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-25 11:02:59 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:02:59 up  2:45,  1 user,  load average: 1.48, 1.01, 0.75

--- npc/csrc/memory/paddr.cpp
+++ npc/csrc/memory/paddr.cpp
@@ -7,7 +7,7 @@ uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
 /* 访存相关 */
-uint64_t paddr_read(uint64_t addr, int len) {
+uint64_t paddr_read(paddr_t addr, int len) {
   uint64_t ret = host_read(guest_to_host(addr), len);
   return ret;
 } 
