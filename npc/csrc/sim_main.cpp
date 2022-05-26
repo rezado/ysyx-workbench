2707b64f90a1b0341397567b4f56d23fbf4233bf
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-26 08:30:44 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:30:44 up  4:47,  1 user,  load average: 0.43, 0.45, 0.39

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -1,6 +1,6 @@
 #include <common.h> 
 #include <memory/paddr.h>
-#include <cpu.h>
+#include <cpu/cpu.h>
 
 /* 全局变量定义 声明 */
 
@@ -9,7 +9,10 @@ VerilatedContext* contextp = NULL;
 VerilatedVcdC* tfp = NULL;
 
 /* 函数声明 */
-void init_sdb();
+void init_monitor(int, char *[]);
+void am_init_monitor();
+void engine_start();
+int is_exit_status_bad();
 
 /* 仿真开始结束相关 */
 
@@ -18,31 +21,13 @@ void single_cycle() {
     top->clk = 1; top->eval(); contextp->timeInc(1); tfp->dump(contextp->time());
 }
 
-extern "C" void init_disasm(const char *triple);
-void sim_init(char *arg) {
+void sim_init() {
   contextp = new VerilatedContext;
   tfp = new VerilatedVcdC;
   top = new Vtop;
   contextp->traceEverOn(true);
   top->trace(tfp, 0);
   tfp->open("dump.vcd");
-	printf("image form:%s\n", arg);
-
-	FILE *fp = fopen(arg, "rb");
-	assert(fp);
-	fseek(fp, 0, SEEK_END);
-	long size = ftell(fp);
-	fseek(fp, 0, SEEK_SET);
-	int ret = fread(guest_to_host(CONFIG_MBASE), size, 1, fp);
-	assert(ret == 1);
-	fclose(fp);
-	// for (int i = 0; i < size; i++)
-	// 	printf("%c ", pmem[i]);
-	// printf("\n");
-
-  init_sdb();
-
-  init_disasm("riscv64" "-pc-linux-gnu");
 }
 
 void sim_exit() {
@@ -52,15 +37,20 @@ void sim_exit() {
     delete tfp;
 }
 
-extern void sdb_mainloop();
-
 int main(int argc, char *argv[]) {
-  assert(argv[1]);
-  sim_init(argv[1]);
-
+  sim_init();
   reset(4);
 
-  sdb_mainloop();
+  /* Initialize the monitor. */
+#ifdef CONFIG_TARGET_AM
+  am_init_monitor();
+#else
+  init_monitor(argc, argv);
+#endif
+
+  /* Start engine. */
+  engine_start();
 
   sim_exit();
+  return 0;
 }
--- npc/include/reg.h
+++
@@ -1,6 +0,0 @@
-#include <common.h>
-
-extern uint64_t *cpu_gpr;
-extern "C" void set_gpr_ptr(const svOpenArrayHandle r);
-void dump_gpr();
-word_t isa_reg_str2val(const char *s, bool *success);
