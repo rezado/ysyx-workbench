38ee0064983c337d9cf545a19eec3614a1ea95c6
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-27 10:40:05 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:40:05 up  7:08,  1 user,  load average: 1.24, 0.63, 0.38

--- npc/include/cpu/cpu.h
+++ npc/include/cpu/cpu.h
@@ -7,6 +7,8 @@ void cpu_exec(uint64_t n);
 
 void set_nemu_state(int state, vaddr_t pc, int halt_ret);
 void invalid_inst(vaddr_t thispc);
+void reset(int n);
+void finish_sim();
 
 #define NEMUTRAP(thispc, code) set_nemu_state(NEMU_END, thispc, code)
 #define INV(thispc) invalid_inst(thispc)
