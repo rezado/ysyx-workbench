81e0cd9b1e151b60a5c4b11e1d0f5231f8537985
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-27 14:16:19 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:16:19 up  8:37,  1 user,  load average: 0.28, 0.39, 0.36

--- npc/scripts/veril.mk
+++ npc/scripts/veril.mk
@@ -37,7 +37,7 @@ sim:
 	$(call git_commit, "sim RTL") # DO NOT REMOVE THIS LINE!!!
 	@echo "Write this Makefile by your self."
 	$(VERILATOR) $(VERILATOR_SIM_CFLAGS) --top-module $(TOPNAME) $(VSRCS) $(CSRCS) $(addprefix -LDFLAGS , $(LDFLAGS)) \
-		$(addprefix -CFLAGS , $(CFLAGS)) --Mdir $(OBJ_DIR)
+		$(addprefix -CFLAGS , $(CFLAGS))
 
 include ../Makefile
 .PHONY: clean run
