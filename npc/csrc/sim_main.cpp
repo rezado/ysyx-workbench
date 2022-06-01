86612b262c06da25f28c0221b01bdcc259aa2b19
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-01 10:45:45 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:45:45 up  1:33,  1 user,  load average: 0.86, 0.50, 0.35

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -15,7 +15,7 @@ void init_sdb();
 
 void single_cycle() {
     top->clk = 0; top->eval(); puts("1"); contextp->timeInc(1); puts("2"); tfp->dump(contextp->time()); puts("3");
-    top->clk = 1; top->eval(); contextp->timeInc(1); tfp->dump(contextp->time());
+    puts("4");top->clk = 1; top->eval(); contextp->timeInc(1); tfp->dump(contextp->time());
 }
 
 extern "C" void init_disasm(const char *triple);
