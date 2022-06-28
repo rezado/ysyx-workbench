d36a41de94817432f728fea427658c0bb9374d18
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-28 20:55:14 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:55:14 up  1:40,  1 user,  load average: 0.46, 0.33, 0.35

--- npc/csrc/memory/paddr.cpp
+++ npc/csrc/memory/paddr.cpp
@@ -61,14 +61,16 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  // if (likely(in_pmem(addr))) return pmem_read(addr, len);
-  // IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
-  // out_of_bound(addr);
-  // return 0;
+  if (likely(in_pmem(addr))) {
+    return host_read(guest_to_host(addr), len);
+  }
+  IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
+  out_of_bound(addr);
+  return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  // if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
-  // IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
-  // out_of_bound(addr);
+  if (likely(in_pmem(addr))) { host_write(guest_to_host(addr), len, data); return; }
+  IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
+  out_of_bound(addr);
 }
