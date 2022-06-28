8db7fc5e1b997c3902fbc4c80aa4a0a1030f8906
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-28 20:42:14 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:42:14 up  1:27,  1 user,  load average: 0.08, 0.33, 0.38

--- npc/csrc/memory/paddr.cpp
+++ npc/csrc/memory/paddr.cpp
@@ -12,16 +12,28 @@ static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
-static word_t pmem_read(paddr_t addr, int len) {
-  word_t ret = host_read(guest_to_host(addr), len);
+extern "C" void pmem_read(long long raddr, long long *rdata) {
+  // 总是读取地址为`raddr & ~0x7ull`的8字节返回给`rdata`
+  // printf("read from %llx\n", raddr);
+  *rdata = host_read(guest_to_host(raddr & ~0x7ull), 8);
   #ifdef CONFIG_MTRACE
     Log("Read Memory at " FMT_PADDR "  data:" FMT_WORD, addr, ret);
   #endif
-  return ret;
 }
 
-static void pmem_write(paddr_t addr, int len, word_t data) {
-  host_write(guest_to_host(addr), len, data);
+extern "C" void pmem_write(long long waddr, long long wdata, char wmask) {
+  // 总是往地址为`waddr & ~0x7ull`的8字节按写掩码`wmask`写入`wdata`
+  // `wmask`中每比特表示`wdata`中1个字节的掩码,
+  // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
+  waddr = waddr & ~0x7ull;
+  for (int i = 0; i < 8; i++) {
+    if (wmask & 1 == 1) {
+      host_write(guest_to_host(waddr), 1, wdata);
+    }
+    wmask >>= 1;
+    waddr += 1;
+    wdata >>= 4;
+  }
   #ifdef CONFIG_MTRACE
     Log("Write Memory at " FMT_PADDR "  data:" FMT_WORD, addr, data);
   #endif
@@ -49,14 +61,14 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
-  IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
-  out_of_bound(addr);
-  return 0;
+  // if (likely(in_pmem(addr))) return pmem_read(addr, len);
+  // IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
+  // out_of_bound(addr);
+  // return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
-  IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
-  out_of_bound(addr);
+  // if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
+  // IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
+  // out_of_bound(addr);
 }
