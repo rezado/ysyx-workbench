bdef4c5a323854a5243fb2db6260b11c9d6e1a02
rezado <1908819406@qq.com>
2022-06-28 21:07:42 +0800

    inst: load store

--- npc/csrc/memory/paddr.cpp
+++ npc/csrc/memory/paddr.cpp
@@ -62,11 +62,7 @@ void init_mem() {
 
 word_t paddr_read(paddr_t addr, int len) {
   if (likely(in_pmem(addr))) {
-    long long tmp;
-    pmem_read(addr, &tmp);
-    switch(case) {
-      case 1: return 
-    }
+    return host_read(guest_to_host(addr), len);
   }
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
@@ -74,7 +70,7 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
+  if (likely(in_pmem(addr))) { host_write(guest_to_host(addr), len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
