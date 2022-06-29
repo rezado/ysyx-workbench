d042546aec4c2dc15bbf93196d897638b8fbb80f
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-29 22:29:35 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:29:35 up 13:31,  1 user,  load average: 0.70, 0.53, 0.40

--- npc/tools/difftest.mk
+++ npc/tools/difftest.mk
@@ -4,10 +4,5 @@ DIFF_REF_SO = $(DIFF_REF_PATH)/build/$(GUEST_ISA)-$(call remove_quote,$(CONFIG_D
 MKFLAGS = GUEST_ISA=$(GUEST_ISA) SHARE=1 ENGINE=interpreter
 ARGS_DIFF = --diff=$(DIFF_REF_SO)
 
-ifndef CONFIG_DIFFTEST_REF_NEMU
-$(DIFF_REF_SO):
-	$(MAKE) -s -C $(DIFF_REF_PATH) $(MKFLAGS)
-endif
-
 .PHONY: $(DIFF_REF_SO)
 endif
