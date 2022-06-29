ac7920e42e78d9b9905ade78ac0f8c1f578f9763
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-29 20:51:12 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:51:12 up 12:10,  1 user,  load average: 1.00, 0.82, 0.52

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -12,10 +12,11 @@ const char *regs1[] = {
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   int i;
   for (i = 0; i < 32; i++) {
-    if (ref_r->gpr[i] != gpr(i))
+    if (ref_r->gpr[i] != gpr(i)) {
       Log("Difftest error at PC: 0x%08lx", pc);
-      printf("reg:%s different\tref:%lx   dut:%lx\n", regs1[i], ref_r->gpr[i], gpr(i));
+      Log("reg:%s different\tref:%lx   dut:%lx\n", regs1[i], ref_r->gpr[i], gpr(i));
       return false;
+    }
   }
   if (ref_r->pc != pc)  {
     printf("refpc:" FMT_WORD "pc:" FMT_WORD "\n", ref_r->pc, pc);
