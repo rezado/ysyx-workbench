ec30b9610e1e01d891b13176f073be40cc61470b
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-30 11:12:03 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:12:03 up 17:10,  1 user,  load average: 0.63, 0.84, 0.64

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -39,13 +39,13 @@ void difftest_regcpy(void *dut, bool direction) {
     diff_set_regs(dut);
   } else {
     puts("DIFFTEST_TO_DUT");
-    // for (int i = 0; i < 32; i++) {
-    //   printf("%lx\n", cpu.gpr[i]);
-    // }
+    for (int i = 0; i < 32; i++) {
+      printf("1%lx\n", cpu.gpr[i]);
+    }
     diff_context_t* p = (diff_context_t*)dut;
     puts("intoi");
     for (int i = 0; i < 32; i++) {
-      printf("%lx", cpu.gpr[i]);
+      printf("2%lx", cpu.gpr[i]);
       // p->gpr[i] = cpu.gpr[i];
     }
     p->pc = cpu.pc;
