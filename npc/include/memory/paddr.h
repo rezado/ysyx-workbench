e038adf2e69053b4d7e0489a3d38d0f1f8eb20d8
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-30 21:27:58 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:27:58 up 23:17,  1 user,  load average: 0.39, 0.30, 0.32

--- npc/include/memory/paddr.h
+++ npc/include/memory/paddr.h
@@ -11,7 +11,8 @@ uint8_t* guest_to_host(paddr_t paddr);
 paddr_t host_to_guest(uint8_t *haddr);
 
 static inline bool in_pmem(paddr_t addr) {
-  return (addr >= 0x70000000) && (addr < (paddr_t)CONFIG_MBASE + CONFIG_MSIZE);
+  printf("%lx\n", addr);
+  return (addr >= 0x80000000) && (addr < (paddr_t)CONFIG_MBASE + CONFIG_MSIZE);
 }
 
 word_t paddr_read(paddr_t addr, int len);
