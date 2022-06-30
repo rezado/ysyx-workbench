7668786aacb94ae9312823f4e02dd29927850ecb
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-30 14:47:11 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:47:11 up 17:54,  1 user,  load average: 1.18, 0.71, 0.58

--- npc/csrc/cpu/difftest/dut.cpp
+++ npc/csrc/cpu/difftest/dut.cpp
@@ -77,6 +77,7 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
   ref_difftest_init(port);
   ref_difftest_memcpy(RESET_VECTOR, guest_to_host(RESET_VECTOR), img_size, DIFFTEST_TO_REF);
   ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
+  printf("init finish\n");
 }
 
 static void checkregs(CPU_state *ref, vaddr_t pc) {
