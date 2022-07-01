1510c9f62f558c1734492172c83d67007d1c91b9
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-01 14:30:38 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:30:38 up 1 day,  3:50,  1 user,  load average: 0.25, 0.42, 0.36

--- npc/csrc/isa/riscv64/difftest/isa_dut.cpp
+++ npc/csrc/isa/riscv64/difftest/isa_dut.cpp
@@ -11,7 +11,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
       Log("reg:%s ref:%lx dut:%lx", regs[i], ref_r->gpr[i], gpr(i));
       printf("\tref\t\t\tdut\n");
       for (int i = 0; i < 32; i++) {
-        printf("%-4s%016lx\t%016lx\n", regs[i], ref_r->gpr[i], gpr(i));
+        printf("%-4s%016llx\t%016llx\n", regs[i], ref_r->gpr[i], gpr(i));
       }
       return false;
     }
