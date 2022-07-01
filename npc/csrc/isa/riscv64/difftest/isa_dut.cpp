deb0c535781aeb1f2a266063a351057f9fa16d64
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-01 11:32:15 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:32:15 up 1 day,  2:54,  1 user,  load average: 1.33, 1.10, 0.85

--- npc/csrc/isa/riscv64/difftest/isa_dut.cpp
+++ npc/csrc/isa/riscv64/difftest/isa_dut.cpp
@@ -5,9 +5,11 @@
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   int i;
   for (i = 0; i < 32; i++) {
-    printf("%lx %lx\n",ref_r->gpr[i], gpr(i));
-    if (ref_r->gpr[i] != gpr(i))
+    if (ref_r->gpr[i] != gpr(i)) {
+      Log("Difftest error at PC: 0x%08lx", pc);
+      Log("reg:%s ref:%lx dut:%lx", regs1[i], ref_r->gpr[i], gpr(i));
       return false;
+    }
   }
   if (ref_r->pc != pc)  {
     printf("refpc:" FMT_WORD "pc:" FMT_WORD "\n", ref_r->pc, pc);
