7720b01442025eb001bcb499cf18c39364d0ecc6
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-01 14:34:45 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:34:45 up 1 day,  3:54,  1 user,  load average: 0.71, 0.58, 0.43

--- npc/csrc/isa/riscv64/difftest/isa_dut.cpp
+++ npc/csrc/isa/riscv64/difftest/isa_dut.cpp
@@ -7,11 +7,11 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   int i;
   for (i = 0; i < 32; i++) {
     if (ref_r->gpr[i] != gpr(i)) {
-      Log("Difftest error at PC: 0x%08lx", pc);
-      Log("reg:%s ref:%lx dut:%lx", regs[i], ref_r->gpr[i], gpr(i));
+      Log("Difftest error at PC:" FMT_PADDR, pc);
+      Log("reg:%s ref:" FMT_WORD "dut:" FMT_WORD, regs[i], ref_r->gpr[i], gpr(i));
       printf("\tref\t\t\tdut\n");
       for (int i = 0; i < 32; i++) {
-        printf("%-4s"FMT_WORD"\t "FMT_WORD"\n", regs[i], ref_r->gpr[i], gpr(i));
+        printf("%-4s" FMT_WORD "\t " FMT_WORD "\n", regs[i], ref_r->gpr[i], gpr(i));
       }
       return false;
     }
