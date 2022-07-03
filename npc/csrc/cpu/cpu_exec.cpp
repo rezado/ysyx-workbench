8eae3ab3b62aa2f2948cb89f3ac9d292b03acde1
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-03 10:33:22 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:33:22 up 17:33,  1 user,  load average: 0.71, 0.60, 0.39

--- npc/csrc/cpu/cpu_exec.cpp
+++ npc/csrc/cpu/cpu_exec.cpp
@@ -95,7 +95,7 @@ static void exec_once(Decode *s) {
   // puts(logbuf);
   
   disassemble(p, logbuf + sizeof(logbuf) - p,
-      s->dnpc, (uint8_t *)&instr.val, ilen);
+      top->pc, (uint8_t *)&instr.val, ilen);
   
   puts(logbuf);
   // iringbuf
