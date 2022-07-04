06d856540d67d1802835b5ca1ed4739aae1d9841
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-04 12:18:02 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:18:02 up  5:31,  1 user,  load average: 0.94, 0.57, 0.37

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,44 +5,41 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  panic("Not implemented");
+static void putchar(char ch, char *str, int to_string) {
+  if (to_string) *str++ = ch;
+  else putch(ch);
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
-}
-
-int sprintf(char *out, const char *fmt, ...) {
-  char *pri = out;
-  va_list ap;
-  va_start(ap, fmt);
+static int print(const char *fmt, va_list *ap, char *str, int to_string) {
   size_t i;
   size_t len = strlen(fmt);
   int flag = 0, argDec;
   char op;
   char *argStr = NULL;
   char num[50];
-  int t, cnt;  // 一些临时变量
+  int t, cnt, chnum = 0;  // 临时变量
   for (i = 0; i < len; i++) {
     if (fmt[i] == '%') {
       flag = 1;
     }
     else if (!flag) {
-      *out = fmt[i];
-      out++;
+      putchar(fmt[i], str, to_string);
+      chnum++;
     }
     else {
       flag = 0;
       op = fmt[i];
       if (op == 'd') {
-        argDec = va_arg(ap, int);
+        argDec = va_arg(*ap, int);
         if (argDec < 0) {
-          *out++ = '-';
           argDec = -argDec;
+          putchar('-', str, to_string);
+          chnum++;
         }
-        else if (argDec == 0)
-          *out++ = '0';
+        else if (argDec == 0) {
+          putchar('0', str, to_string);
+          chnum++;
+        }
 
         t = argDec;
         cnt = 0;
@@ -52,23 +49,45 @@ int sprintf(char *out, const char *fmt, ...) {
         }
         
         while (cnt > 0) {
-          *out++ = num[--cnt];
+          putchar(num[--cnt], str, to_string);
+          chnum++;
         }
       }
       else if (op == 's') {
-        argStr = va_arg(ap, char*);
+        argStr = va_arg(*ap, char*);
         while (*argStr != '\0') {
-          *out = *argStr;
-          out++;
+          putchar(*argStr, str, to_string);
           argStr++;
+          chnum++;
         }
       }
     }
   }
-  *out++ = 0;
-  va_end(ap);
+    if(to_string) *str++ = 0;
+    return chnum;
+}
 
-  return out - pri;
+
+int printf(const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  int chnum = 0;
+  chnum = print(fmt, &ap, NULL, false);
+  va_end(ap);
+  return chnum;
+}
+
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  panic("Not implemented");
+}
+
+int sprintf(char *out, const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  int chnum = 0;
+  chnum = print(fmt, &ap, out, true);
+  va_end(ap);
+  return chnum;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
