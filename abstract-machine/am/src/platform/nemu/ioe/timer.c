657f0be9a7e875d4b207b5564e80535d7cdc6b80
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-05 11:11:38 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:11:38 up 12:22,  1 user,  load average: 0.72, 0.63, 0.56

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -8,7 +8,7 @@ void __am_timer_init() {
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   printf("__am_timer_uptime\n");
   uptime->us = inl(RTC_ADDR);
-  printf("us:%lx\n", uptime->us);
+  printf("us:%d\n", uptime->us);
   
   // uptime->us = paddr_read(RTC_ADDR, 4);
 }
