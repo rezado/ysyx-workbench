3cb06429f56e75f5d02f006de4edca1dca984bc0
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-05 22:01:15 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:01:15 up 21:00,  1 user,  load average: 0.99, 0.59, 0.41

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,8 +5,8 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   kbd->keycode = inl(KBD_ADDR);
-  if ((kbd->keycode & KEYDOWN_MASK) != 0)
-    kbd->keydown = 1;
-  else
-    kbd->keydown = 0;
+  // if ((kbd->keycode & KEYDOWN_MASK) != 0)
+  //   kbd->keydown = 1;
+  // else
+  //   kbd->keydown = 0;
 }
