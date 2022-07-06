516e0e398df01a6e6fafce07d0592031d324ef7a
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-06 20:58:48 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:58:48 up 1 day,  6:10,  3 users,  load average: 0.46, 0.44, 0.27

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -30,6 +30,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
     for (int x = ctl->x; x < ctl->x + ctl->w; x++) {
       for (int y = ctl->y; y < ctl->y + ctl->h; y++) {
         outl(addr++, *(uint32_t*)ctl->pixels++);
+        printf("write addr:%x %d\n", addr, *(uint32_t*)ctl->pixels);
       }
       addr = addr + width - ctl->w;
     }
