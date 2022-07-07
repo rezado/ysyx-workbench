2f99d420024a531c331e14e383303e2434a76944
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-07 17:35:21 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:35:21 up  5:07,  3 users,  load average: 0.66, 0.88, 0.85

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -20,26 +20,27 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
-  // int width = inl(VGACTL_ADDR) >> 16;
-  // uintptr_t addr = FB_ADDR + ctl->x * width + ctl->y; 
-  // int col = ctl->h;
-  // int row = ctl->w;
-  // uint32_t *p = (uint32_t*)ctl->pixels;
-  // for (int i = 0; i < row; i++) {
-  //   for (int j = 0; j < col; j++) {
-  //     outl(addr++, *p++);
-  //   }
-  //   addr = addr + width - row; 
-  // }
   int width = inl(VGACTL_ADDR) >> 16;
-  int col = ctl->h, row = ctl->w;
-  int offset = 0;
-  for (int x = ctl->x; x < ctl->x + row; x++) {
-    for (int y = ctl->y; y < ctl->y + col; y++) {
-      offset = y * width + x;
-      outl(FB_ADDR + offset, *(uint32_t*)(ctl->pixels++));
+  uintptr_t addr = FB_ADDR + ctl->y * width + ctl->x; 
+  int col = ctl->h;
+  int row = ctl->w;
+  uint32_t *p = (uint32_t*)ctl->pixels;
+  for (int i = 0; i < row; i++) {
+    for (int j = 0; j < col; j++) {
+      outl(addr++, *p++);
     }
   }
+  //   addr = addr + width - row; 
+  // }
+  // int width = inl(VGACTL_ADDR) >> 16;
+  // int col = ctl->h, row = ctl->w;
+  // int offset = 0;
+  // for (int x = ctl->x; x < ctl->x + row; x++) {
+  //   for (int y = ctl->y; y < ctl->y + col; y++) {
+  //     offset = y * width + x;
+  //     outl(FB_ADDR + offset, *(uint32_t*)(ctl->pixels++));
+  //   }
+  // }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
