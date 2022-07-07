f464b3d1d09f2fa772afd54e48ce043087cfd7b6
rezado <1908819406@qq.com>
2022-07-07 22:16:36 +0800

    fix:VGA debug

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,5 +1,6 @@
 #include <am.h>
 #include <nemu.h>
+// #include <stdio.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
@@ -20,14 +21,26 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
     outl(SYNC_ADDR, 1);
   }
   int width = inl(VGACTL_ADDR) >> 16;
-  uintptr_t addr = FB_ADDR + ctl->x * width + ctl->y;
-  for (int x = ctl->x; x < ctl->x + ctl->w; x++) {
-    for (int y = ctl->y; y < ctl->y + ctl->h; y++) {
-      outl(addr++, *(uint32_t*)ctl->pixels++);
-      // printf("write addr:%x %d\n", addr, *(uint32_t*)ctl->pixels);
+  uintptr_t addr = FB_ADDR; 
+  int w = ctl->w, h = ctl->h;
+  int x = ctl->x, y = ctl->y;
+  uint32_t *p = (uint32_t*)ctl->pixels;
+  for (int i = 0; i < h; i++) {
+    for (int j = 0; j < w; j++) {
+      addr = FB_ADDR + ((y + i) * width + (x + j)) * 4;
+      // printf("addr:%d\n", addr);
+      outl(addr, *p++);
     }
-    addr = addr + width - ctl->w;
   }
+  // int width = inl(VGACTL_ADDR) >> 16;
+  // int col = ctl->h, row = ctl->w;
+  // int offset = 0;
+  // for (int x = ctl->x; x < ctl->x + row; x++) {
+  //   for (int y = ctl->y; y < ctl->y + col; y++) {
+  //     offset = y * width + x;
+  //     outl(FB_ADDR + offset, *(uint32_t*)(ctl->pixels++));
+  //   }
+  // }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -5,7 +5,7 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
 
-static void *addr = NULL;
+static char *hbrk = NULL;
 
 int rand(void) {
   // RAND_MAX assumed to be 32767
@@ -36,19 +36,21 @@ void *malloc(size_t size) {
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 
-  if (!addr) addr = heap.start;
-  void *pri = addr;
-  addr = pri + size;
-  if (size == 0)
+  #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+    if (!hbrk) hbrk = (void*)ROUNDUP(heap.start, 8);
+    size = (size_t)ROUNDUP(size, 8);
+    // printf("size:%d\n", size);
+    assert((size & 1) == 0);
+    char *old = hbrk;
+    hbrk += size;
+    assert((uintptr_t)heap.start <= (uintptr_t)hbrk && (uintptr_t)hbrk < (uintptr_t)heap.end);
+    for (uint64_t *p = (uint64_t *)old; p != (uint64_t *)hbrk; p++) {
+      *p = 0;
+    }
+    assert((uintptr_t)hbrk <= (uintptr_t)heap.end);
+    return old;
+  #endif
     return NULL;
-  else if (addr > heap.end) {
-    addr = pri;
-    return NULL;
-  }
-  else {
-    addr = addr + ((addr - heap.start + 8) / 8 * 8);
-    return pri;
-  }
 }
 
 void free(void *ptr) {
