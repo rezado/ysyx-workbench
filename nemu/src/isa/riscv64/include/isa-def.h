513d48ab9828860f00585c04d8fbaf5a896bb12e
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-11 22:36:47 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:36:47 up 13:57,  3 users,  load average: 0.36, 0.29, 0.22

--- nemu/src/isa/riscv64/include/isa-def.h
+++ nemu/src/isa/riscv64/include/isa-def.h
@@ -16,6 +16,27 @@ typedef struct {
   } inst;
 } riscv64_ISADecodeInfo;
 
+enum {
+  ECALL = 0
+};
+
+// CSR
+enum {
+  MEPC = 0,
+  MCAUSE, MSTATUS, MTVEC
+};
+
+static inline uint32_t getcsr(uint32_t num) {
+  switch(num) {
+    case 0x341: return MEPC;
+    case 0x342: return MCAUSE;
+    case 0x300: return MSTATUS;
+    case 0x305: return MTVEC;
+    default: assert(0);
+  }
+}
+
+#define csr(idx) (cpu.csr[getcsr(idx)])
 
 #define isa_mmu_check(vaddr, len, type) (MMU_DIRECT)
 
--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -1,12 +1,17 @@
 #include <isa.h>
 
+ word_t mc[20] = {
+   0x0000000b
+ };
+
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-  
+  csr(MEPC) = epc;  // 設定爲ecall指令本身PC
+  csr(MCAUSE) = mc[NO];
 
-  return 0;
+  return csr(MTVEC);
 }
 
 word_t isa_query_intr() {
