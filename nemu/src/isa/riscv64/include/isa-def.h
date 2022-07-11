9e83178dc5e7d6ffbbccb2df2db6d24d357a97c5
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-11 22:40:25 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:40:25 up 14:01,  3 users,  load average: 1.16, 0.63, 0.37

--- nemu/src/isa/riscv64/include/isa-def.h
+++ nemu/src/isa/riscv64/include/isa-def.h
@@ -26,13 +26,13 @@ enum {
   MCAUSE, MSTATUS, MTVEC
 };
 
-static inline uint32_t getcsr(uint32_t num) {
+static inline int32_t getcsr(uint32_t num) {
   switch(num) {
     case 0x341: return MEPC;
     case 0x342: return MCAUSE;
     case 0x300: return MSTATUS;
     case 0x305: return MTVEC;
-    default: assert(0);
+    default: Assert(0, "get wrong csr num:%d\n", num);
   }
 }
 
