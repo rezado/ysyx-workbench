10dcbe4d2c6e5e78de02e9c32f81b092a761b1a1
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-13 15:00:05 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:00:05 up  7:23,  1 user,  load average: 0.51, 0.32, 0.19

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -1,8 +1,12 @@
 #include <isa.h>
 
- word_t mc[20] = {
-   0x0000000b
- };
+word_t mc[20] = {
+  0x0000000b
+};
+
+static const char *mcause[] = {
+  "Ecall from M-mode"
+};
 
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
@@ -10,6 +14,10 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
    */
   cpu.csr[MEPC] = epc;  // 設定爲ecall指令本身PC
   cpu.csr[MCAUSE] = mc[NO];
+
+#ifdef CONFIG_ETRACE
+  Log("ETACE: oldPC " FMT_WORD "  newPC " FMT_WORD "\nmcause: %s", epc, cpu.csr[MTVEC], mcause[NO]);
+#endif
 
   return cpu.csr[MTVEC];
 }
