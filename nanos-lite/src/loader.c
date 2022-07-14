d29b5049cd2671c0eb700bc32688e88b803a2385
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-14 10:01:55 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:01:55 up 13:36,  5 users,  load average: 0.10, 0.18, 0.27

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,9 +9,45 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+extern uint8_t ramdisk_start;
+extern uint8_t ramdisk_end;
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  // 读取ELF文件头
+  Elf_Ehdr *Ehdr = NULL;
+  size_t ret;
+  Ehdr = (Elf_Ehdr*)malloc(sizeof(Elf_Ehdr));
+  ret = ramdisk_read(Ehdr, 0, sizeof(Elf_Ehdr));
+  assert(ret);
+
+  // 读取program header table
+  Elf_Phdr *Phdrs = NULL;
+  Phdrs = (Elf_Phdr*)malloc(sizeof(Elf_Phdr) * Ehdr->e_phnum);
+  ret = ramdisk_read(Phdrs, Ehdr->e_phoff, sizeof(Elf_Phdr) * Ehdr->e_phnum);
+  assert(ret);
+
+  // 遍历program headers
+  char ch = '\0';
+  for (uint32_t i = 0; i < Ehdr->e_phnum; i++) {
+    if (Phdrs[i].p_type == PT_LOAD) {
+      ret = ramdisk_write(&ramdisk_start + Phdrs[i].p_offset, Phdrs[i].p_vaddr, Phdrs[i].p_filesz);
+      // memcpy(&ramdisk_start + vaddr, &ramdisk_start + p_offset, filesiz)
+      assert(ret);
+      for (uint32_t p = Phdrs[i].p_filesz; p < Phdrs[i].p_memsz; p++) {
+        ret = ramdisk_write(&ch, Phdrs[i].p_vaddr + p, 1);
+        assert(ret);
+      }
+    }
+  }
+
+  uintptr_t proc = Ehdr->e_entry;
+  free(Ehdr);
+  free(Phdrs);
+
+  return proc;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
