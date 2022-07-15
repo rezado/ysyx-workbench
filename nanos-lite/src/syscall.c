629e35a813d6af1a2ccc1278ef5fcc170395ce9b
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-15 16:25:54 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:25:54 up 17 min,  1 user,  load average: 0.64, 0.50, 0.42

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,12 @@
 #include <common.h>
 #include "syscall.h"
+const char *syscname[] = {
+  "SYS_exit", "SYS_yield", "SYS_open", "SYS_read", "SYS_write", "SYS_kill",
+  "SYS_getpid", "SYS_close", "SYS_lseek", "SYS_brk", "SYS_fstat", "SYS_time",
+  "SYS_signal", "SYS_execve", "SYS_fork", "SYS_link", "SYS_unlink",
+  "SYS_wait", "SYS_times", "SYS_gettimeofday"
+};
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -7,9 +14,10 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
 
+
   switch (a[0]) {
-    case SYS_yield: yield(); break;
-    case SYS_exit: halt(a[0]); break;
+    case SYS_yield: Log("Syscall: yield()\n"); yield(); break;
+    case SYS_exit: Log("Syscall: exit(%x)", a[0]); halt(a[0]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
