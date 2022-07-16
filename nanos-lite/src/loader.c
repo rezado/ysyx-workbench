3d338b3c2c522a0cf13a116fbfb9a6a179a1a465
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-16 20:38:16 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:38:16 up  7:26,  3 users,  load average: 0.10, 0.08, 0.09

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include <proc.h>
 #include <elf.h>
+#include <fs.h>
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -26,36 +27,41 @@ size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
+  // 读取指定文件
+  int fd = fs_open(filename, 0, 0);
+  assert(fd != -1);
+
   // 读取ELF文件头
   Elf_Ehdr *Ehdr = NULL;
   size_t ret;
   Ehdr = (Elf_Ehdr*)malloc(sizeof(Elf_Ehdr));
-  ret = ramdisk_read(Ehdr, 0, sizeof(Elf_Ehdr));
+  // ret = ramdisk_read(Ehdr, 0, sizeof(Elf_Ehdr));
+  ret = fs_read(fd, Ehdr, sizeof(Elf_Ehdr));
   assert(ret);
-
   // 检查魔数
   assert(*(uint32_t*)Ehdr->e_ident == 0x464c457f);
   // 检查架构
   assert(Ehdr->e_machine == EXPECT_TYPE);
-
-  // 读取program header table
-  Elf_Phdr *Phdrs = NULL;
-  Phdrs = (Elf_Phdr*)malloc(sizeof(Elf_Phdr) * Ehdr->e_phnum);
-  ret = ramdisk_read(Phdrs, Ehdr->e_phoff, sizeof(Elf_Phdr) * Ehdr->e_phnum);
-  assert(ret);
-
-  // 遍历program headers
-  for (uint32_t i = 0; i < Ehdr->e_phnum; i++) {
-    if (Phdrs[i].p_type == PT_LOAD) {
-      ret = ramdisk_read((void*)Phdrs[i].p_vaddr, Phdrs[i].p_offset, Phdrs[i].p_filesz);
-      assert(ret);
-      memset((void*)(Phdrs[i].p_vaddr + Phdrs[i].p_filesz), 0, Phdrs[i].p_memsz - Phdrs[i].p_filesz);
-    }
-  }
-
+  // 设定返回值
   uintptr_t proc = Ehdr->e_entry;
+
+  // // 读取program header table
+  // Elf_Phdr *Phdrs = NULL;
+  // Phdrs = (Elf_Phdr*)malloc(sizeof(Elf_Phdr) * Ehdr->e_phnum);
+  // ret = ramdisk_read(Phdrs, Ehdr->e_phoff, sizeof(Elf_Phdr) * Ehdr->e_phnum);
+  // assert(ret);
+
+  // // 遍历program headers
+  // for (uint32_t i = 0; i < Ehdr->e_phnum; i++) {
+  //   if (Phdrs[i].p_type == PT_LOAD) {
+  //     ret = ramdisk_read((void*)Phdrs[i].p_vaddr, Phdrs[i].p_offset, Phdrs[i].p_filesz);
+  //     assert(ret);
+  //     memset((void*)(Phdrs[i].p_vaddr + Phdrs[i].p_filesz), 0, Phdrs[i].p_memsz - Phdrs[i].p_filesz);
+  //   }
+  // }
+
   free(Ehdr);
-  free(Phdrs);
+  // free(Phdrs);
 
   return proc;
 }
