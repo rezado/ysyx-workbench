31d70ed17b0618fdc91203985cd215cdadf22a46
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-17 22:40:18 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:40:18 up  2:31,  3 users,  load average: 0.60, 0.37, 0.32

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,13 +3,6 @@
 
 #define ETRACE
 
-const char *syscname[] = {
-  "SYS_exit", "SYS_yield", "SYS_open", "SYS_read", "SYS_write", "SYS_kill",
-  "SYS_getpid", "SYS_close", "SYS_lseek", "SYS_brk", "SYS_fstat", "SYS_time",
-  "SYS_signal", "SYS_execve", "SYS_fork", "SYS_link", "SYS_unlink",
-  "SYS_wait", "SYS_times", "SYS_gettimeofday"
-};
-
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
