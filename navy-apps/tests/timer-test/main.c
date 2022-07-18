8ec1e1cdea05a04ff549fd0ca01ad639628f0542
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-18 20:14:38 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:14:38 up 27 min,  5 users,  load average: 0.72, 0.38, 0.40

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <assert.h>
-#include <sys/time.h>
+#include "NDL.h"
 
 int main() {
-    struct timeval tv;
-    int ret = -1, usec = 0;
+    int usec = 0, ret;
     while (1) {
-        ret = gettimeofday(&tv, NULL);
-        assert(ret == 0);
-        if (tv.tv_usec - usec >= 500000) {
+        ret = NDL_GetTicks();
+        if (ret - usec >= 500000) {
             printf("0.5秒过去了 有没有好好做PA呢\n");
-            usec = tv.tv_usec;
+            usec = ret;
         }
     }
 
