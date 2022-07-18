7d5080a1bd0b1dbfb50b33cf6b5ec37122327216
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-18 10:19:28 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:19:28 up  4:17,  5 users,  load average: 0.92, 0.60, 0.45

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -5,7 +5,7 @@
 #include <assert.h>
 #include <time.h>
 #include "syscall.h"
-// #include <stdio.h>
+#include <stdio.h>
 
 // helper macros
 #define _concat(x, y) x ## y
@@ -89,7 +89,7 @@ void *_sbrk(intptr_t increment) {
 }
 
 int _read(int fd, void *buf, size_t count) {
-  // printf("_read(%d,%p,%ld)\n", fd, buf, count);
+  printf("_read(%d,%p,%ld)\n", fd, buf, count);
   size_t ret = _syscall_(SYS_read, fd, (intptr_t)buf, count);
   return ret;
 }
