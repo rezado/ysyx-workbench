1d939d967439993bb26404c990387604f303ae32
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-18 10:26:51 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:26:51 up  4:24,  5 users,  load average: 0.36, 0.54, 0.50

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -42,25 +42,25 @@ void do_syscall(Context *c) {
       printf("read:%d\n", a[1]);
       c->GPRx = fs_read(a[1], (void*)a[2], a[3]);
       #ifdef ETRACE
-        Log("Syscall: read(%s, %x, %ld) = %ld", (char*)a[1], a[2], a[3], c->GPRx);
+        Log("Syscall: read(%d, %x, %ld) = %ld", a[1], a[2], a[3], c->GPRx);
       #endif
       break;
     case SYS_write:
       c->GPRx = fs_write(a[1], (void*)a[2], a[3]);
       #ifdef ETRACE
-        if (a[1] > 2) Log("Syscall: wirte(%s, %x, %ld) = %ld", (char*)a[1], a[2], a[3], c->GPRx);
+        if (a[1] > 2) Log("Syscall: wirte(%d, %x, %ld) = %ld", a[1], a[2], a[3], c->GPRx);
       #endif
       break;
     case SYS_close:
       c->GPRx = fs_close(a[1]);
       #ifdef ETRACE
-        Log("Syscall: close(%s) = %x", (char*)a[1], c->GPRx);
+        Log("Syscall: close(%d) = %x", a[1], c->GPRx);
       #endif
       break;
     case SYS_lseek:
       c->GPRx = fs_lseek(a[1], a[2], a[3]);
       #ifdef ETRACE
-        Log("Syscall: lseek(%s, %ld, %d) = %ld", (char*)a[1], a[2], a[3], c->GPRx);
+        Log("Syscall: lseek(%d, %ld, %d) = %ld", a[1], a[2], a[3], c->GPRx);
       #endif
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
