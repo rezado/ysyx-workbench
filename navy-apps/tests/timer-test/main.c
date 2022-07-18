1377fb687fbc4c7349d46a0d2c3e7f5214f3d64e
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-18 17:20:12 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:20:12 up 11:18,  5 users,  load average: 0.64, 0.64, 0.61

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -3,14 +3,14 @@
 #include <sys/time.h>
 
 int main() {
-    struct timeval *tv;
+    struct timeval tv;
     int ret = -1, sec = 0;
     while (1) {
-        ret = gettimeofday(tv, NULL);
+        ret = gettimeofday(&tv, NULL);
         assert(ret == -1);
-        if (tv->tv_sec - sec == 0.5) {
+        if (tv.tv_sec - sec == 0.5) {
             printf("0.5秒过去了 有没有好好做PA呢\n");
-            sec = tv->tv_sec;
+            sec = tv.tv_sec;
         }
     }
 
