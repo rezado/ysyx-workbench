ec56e6006090d1584825870d737bb83bfb699043
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-19 10:37:08 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:37:08 up  4:02,  5 users,  load average: 1.09, 0.76, 0.52

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -25,10 +25,11 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 size_t events_read(void *buf, size_t offset, size_t len) {
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
   int ret = 0;
-  printf("keydown:%d keycode:%d\n", ev.keydown, ev.keycode);
   // 无有效按键
   if (ev.keycode == AM_KEY_NONE || ev.keycode > 255)
     return 0;
+  
+  printf("keydown:%d keycode:%d\n", ev.keydown, ev.keycode);
 
   if (ev.keydown == true) sprintf((char*)buf, "%s", "kd ");
   else sprintf((char*)buf, "%s", "ku ");
