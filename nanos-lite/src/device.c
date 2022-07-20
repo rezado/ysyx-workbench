a8ae0cdd3348c69de0eb9bf184fd3e1b6e3de054
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-20 21:24:00 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:24:00 up  5:36,  4 users,  load average: 0.69, 0.85, 0.70

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -41,7 +41,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  int x = offset % screen_w, y = offset / screen_w;
+  int x = offset / 4 % screen_w, y = offset / 4 / screen_w;
   int num = len / 4;
   int w = num % screen_w, h = 1;
   // printf("fb_write at x:%d y:%d w:%d h:%d\n", x, y, w, h);
