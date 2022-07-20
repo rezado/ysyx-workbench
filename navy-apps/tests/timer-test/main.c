18d0fce92b6475af7db377ccff10cc976d4fca5f
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-20 19:48:00 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:48:00 up  4:00,  4 users,  load average: 0.72, 0.67, 0.46

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -7,9 +7,9 @@ int main() {
     while (1) {
         ret = NDL_GetTicks();
         // printf("%d\n", ret);
-        if (ret - usec >= 500000) {
+        if (ret - usec >= 500000 || ret - usec <= -500000) {
             printf("0.5秒过去了 有没有好好做PA呢\n");
-            usec = 0;
+            usec = ret;
         }
     }
 
