41396c22f691c2315da34e8350d6dbafdbeed690
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-20 16:46:24 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:46:24 up 59 min,  3 users,  load average: 0.99, 0.60, 0.41

--- navy-apps/tests/fix-test/main.c
+++ navy-apps/tests/fix-test/main.c
@@ -4,6 +4,11 @@
 int main(void) {
     fixedpt a = fixedpt_rconst(1.2);
     fixedpt b = fixedpt_fromint(10);
+    fixedpt d = fixedpt_rconst(-1.2);
+    printf("floor 1.2:%d\n", fixedpt_toint(fixedpt_floor(a)));
+    printf("floor -1.2:%d\n", fixedpt_toint(fixedpt_floor(d)));
+    printf("ceil 1.2:%d\n", fixedpt_toint(fixedpt_ceil(a)));
+    printf("ceil -1.2:%d\n", fixedpt_toint(fixedpt_ceil(d)));
     int c = 0;
     if (b > fixedpt_rconst(7.9)) {
         printf("10 > 7.9\n");
