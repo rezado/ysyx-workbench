50bfc33798ee405a3a9a3e84007ffa8640437766
rezado <1908819406@qq.com>
2022-07-21 19:13:27 +0800

    finish SDL video

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,5 +1,6 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <string.h>
 
 #define keyname(k) #k,
 
@@ -13,10 +14,37 @@ int SDL_PushEvent(SDL_Event *ev) {
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
+  char buf[64];
+  int ret = NDL_PollEvent(buf, sizeof(buf));
+  if (ret == 0) return 0;
+  bool keydown;
+  int keycode;
+  char str1[64], str2[64];
+  sscanf(buf, "%s %s\n", str1, str2);
+  if (strcmp(str1, "kd") == 0) keydown = true;
+  else keydown = false;
+
+  int i = 0;
+  while(1) {
+    if (strcmp(str2, keyname[i]) == 0) {
+      keycode = i;
+      break;
+    }
+    i++;
+  }
+
+  ev->type = keydown ? SDL_KEYDOWN : SDL_KEYUP;
+  ev->key.keysym.sym = i;
+  
+  return 1;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
+  int ret = 0;
+  while (1) {
+    ret = SDL_PollEvent(event);
+    if (ret) break;
+  }
   return 1;
 }
 
