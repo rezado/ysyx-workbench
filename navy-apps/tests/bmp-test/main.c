dca3454a3909bb93b4da1e566963ee293224be85
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-21 10:06:25 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:06:25 up  7:55,  4 users,  load average: 0.79, 0.78, 0.59

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -7,7 +7,7 @@
 int main() {
   NDL_Init(0);
   int w, h;
-  void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
+  void *bmp = BMP_Load("share/slides/slides-0.bmp", &w, &h);
   assert(bmp);
   NDL_OpenCanvas(&w, &h);
   printf("width:%d height:%d\n", w, h);
