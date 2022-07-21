b1a8cbbc10572ba415c7554c428dc1779b528633
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-21 10:11:39 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:11:39 up  8:00,  4 users,  load average: 0.89, 0.71, 0.62

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -44,7 +44,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   int x = offset / 4 % screen_w, y = offset / 4 / screen_w;
   int num = len / 4;
   int w = num % screen_w, h = 1;
-  // printf("fb_write at x:%d y:%d w:%d h:%d\n", x, y, w, h);
+  printf("fb_write at x:%d y:%d w:%d h:%d\n", x, y, w, h);
   io_write(AM_GPU_FBDRAW, x, y, (void*)buf, w, h, 1);
   return len;
 }
--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -59,12 +59,12 @@ void NDL_OpenCanvas(int *w, int *h) {
 
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   int fd = open("/dev/fb", 0, 0);
-  // if (x0 == 0 && y0 == 0 && x == 0 && y == 0 && w == screen_w && h == screen_h) {
-  //   // 全屏幕绘图
-  //   printf("into full screen\n");
-  //   write(fd, pixels, w * h * 4);
-  //   return;
-  // }
+  if (x0 == 0 && y0 == 0 && x == 0 && y == 0 && w == screen_w && h == screen_h) {
+    // 全屏幕绘图
+    printf("into full screen\n");
+    write(fd, pixels, w * h * 4);
+    return;
+  }
 
   uint32_t *p = pixels;
   int x1, y1, offset;
