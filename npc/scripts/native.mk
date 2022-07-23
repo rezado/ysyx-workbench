511a037accdf0cdf5355afd6d564533c9fe666e8
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-23 16:02:58 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:02:58 up  6:23,  4 users,  load average: 0.56, 0.36, 0.38

--- npc/scripts/native.mk
+++ npc/scripts/native.mk
@@ -1,18 +1,7 @@
 -include $(NPC_HOME)/../Makefile
 include $(NPC_HOME)/tools/difftest.mk
 
-compile_git:
-	$(call git_commit, "compile NEMU")
-$(BINARY): compile_git
 
-# Some convenient rules
-
-override ARGS ?= --log=$(BUILD_DIR)/npc-log.txt
-override ARGS += $(ARGS_DIFF)
-
-# Command to execute NEMU
-IMG ?=
-NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
 
 clean-tools = $(dir $(shell find ./tools -maxdepth 2 -mindepth 2 -name "Makefile"))
 $(clean-tools):
