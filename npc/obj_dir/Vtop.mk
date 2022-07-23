fb05cf3564848cf0661a777fe595b4253fdab41d
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-23 15:27:17 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:27:17 up  5:48,  4 users,  load average: 0.16, 0.24, 0.26

--- npc/obj_dir/Vtop.mk
+++ npc/obj_dir/Vtop.mk
@@ -35,6 +35,26 @@ VM_PREFIX = Vtop
 VM_MODPREFIX = Vtop
 # User CFLAGS (from -CFLAGS on Verilator command line)
 VM_USER_CFLAGS = \
+	-O2 \
+	-Og \
+	-ggdb3 \
+	-DITRACE_COND=true \
+	-D__GUEST_ISA__=riscv64 \
+	-O2 \
+	-MMD \
+	-Wall \
+	-I/home/bill/ysyx-workbench/npc/include \
+	-I/home/bill/ysyx-workbench/npc/include \
+	-I/home/bill/ysyx-workbench/npc/csrc/engine/interpreter \
+	-I/home/bill/ysyx-workbench/npc/csrc/isa/riscv64/include \
+	-O2 \
+	-MMD \
+	-Wall \
+	-I/home/bill/ysyx-workbench/npc/include \
+	-I/home/bill/ysyx-workbench/npc/include \
+	-I/home/bill/ysyx-workbench/npc/csrc/engine/interpreter \
+	-I/home/bill/ysyx-workbench/npc/csrc/isa/riscv64/include \
+	-DTOP_NAME="Vtop" \
 	-I/usr/lib/llvm-12/include \
 	-std=c++14 \
 	-fno-exceptions \
