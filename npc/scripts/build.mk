cefc7ff609d5c300239d6af350fe12359aa8b23a
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-23 11:35:31 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:35:31 up  4:41,  4 users,  load average: 0.79, 0.68, 0.41

--- npc/scripts/build.mk
+++ npc/scripts/build.mk
@@ -1,35 +1,35 @@
-.DEFAULT_GOAL = app
+# .DEFAULT_GOAL = app
 
-# Add necessary options if the target is a shared library
-ifeq ($(SHARE),1)
-SO = -so
-CFLAGS  += -fPIC
-LDFLAGS += -rdynamic -shared -fPIC
-endif
+# # Add necessary options if the target is a shared library
+# ifeq ($(SHARE),1)
+# SO = -so
+# CFLAGS  += -fPIC
+# LDFLAGS += -rdynamic -shared -fPIC
+# endif
 
-WORK_DIR  = $(shell pwd)
-BUILD_DIR = $(WORK_DIR)/build
+# WORK_DIR  = $(shell pwd)
+# BUILD_DIR = $(WORK_DIR)/build
 
-INC_PATH := $(WORK_DIR)/include $(INC_PATH)
-OBJ_DIR  = $(BUILD_DIR)/obj-$(NAME)$(SO)
-BINARY   = $(BUILD_DIR)/$(NAME)$(SO)
+# INC_PATH := $(WORK_DIR)/include $(INC_PATH)
+# OBJ_DIR  = $(BUILD_DIR)/obj-$(NAME)$(SO)
+# BINARY   = $(BUILD_DIR)/$(NAME)$(SO)
 
-# Compilation flags
-ifeq ($(CC),clang)
-CXX := clang++
-else
-CXX := g++
-endif
-LD := $(CXX)
-INCLUDES = $(addprefix -I, $(INC_PATH))
-CFLAGS  := -O2 -MMD -Wall $(INCLUDES) $(CFLAGS)
-LDFLAGS := -O2 $(LDFLAGS)
-
-
-# Some convenient rules
-
-.PHONY: app clean
+# # Compilation flags
+# ifeq ($(CC),clang)
+# CXX := clang++
+# else
+# CXX := g++
+# endif
+# LD := $(CXX)
+# INCLUDES = $(addprefix -I, $(INC_PATH))
+# CFLAGS  := -O2 -MMD -Wall $(INCLUDES) $(CFLAGS)
+# LDFLAGS := -O2 $(LDFLAGS)
 
 
-clean:
-	-rm -rf $(BUILD_DIR)
+# # Some convenient rules
+
+# .PHONY: app clean
+
+
+# clean:
+# 	-rm -rf $(BUILD_DIR)
