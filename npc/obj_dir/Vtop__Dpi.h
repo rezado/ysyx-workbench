6a23e8a227ebf31adc6aad1954b7c38da69e66e4
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-23 11:36:33 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:36:33 up  4:42,  4 users,  load average: 0.45, 0.60, 0.40

---
+++ npc/obj_dir/Vtop__Dpi.h
@@ -0,0 +1,31 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Prototypes for DPI import and export functions.
+//
+// Verilator includes this file in all generated .cpp files that use DPI functions.
+// Manually include this file where DPI .c import functions are declared to ensure
+// the C functions match the expectations of the DPI imports.
+
+#include "svdpi.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+    // DPI IMPORTS
+    // DPI import at /home/bill/ysyx-workbench/npc/vsrc/top.v:115:30
+    extern void finish_sim();
+    // DPI import at /home/bill/ysyx-workbench/npc/vsrc/top.v:134:30
+    extern void get_inst(int inst);
+    // DPI import at /home/bill/ysyx-workbench/npc/vsrc/top.v:128:30
+    extern void get_inv(int inv);
+    // DPI import at /home/bill/ysyx-workbench/npc/vsrc/top.v:18:30
+    extern void pmem_read(long long raddr, long long* rdata);
+    // DPI import at /home/bill/ysyx-workbench/npc/vsrc/mem.v:13:30
+    extern void pmem_write(long long waddr, long long wdata, char wmask);
+    // DPI import at /home/bill/ysyx-workbench/npc/vsrc/ysyx_22040088_regfile.v:21:30
+    extern void set_gpr_ptr(const svOpenArrayHandle a);
+
+#ifdef __cplusplus
+}
+#endif
