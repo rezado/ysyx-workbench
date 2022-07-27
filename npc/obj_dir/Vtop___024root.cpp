684e2cd693a919fe6c6ebdba809d60ce88170b6d
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-27 14:34:00 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:34:00 up 15:38,  4 users,  load average: 1.11, 0.70, 0.39

--- npc/obj_dir/Vtop___024root.cpp
+++ npc/obj_dir/Vtop___024root.cpp
@@ -98,10 +98,11 @@ VL_INLINE_OPT void Vtop___024root___sequent__TOP__2(Vtop___024root* vlSelf) {
                    : ((4U == (7U & (IData)(vlSelf->top__DOT__pc_out)))
                        ? (IData)((vlSelf->top__DOT__u_ysyx_22040088_IFU__DOT__inst_data 
                                   >> 0x20U)) : 0U)));
-    if ((1U & (~ (IData)(vlSelf->rst)))) {
+    if (VL_UNLIKELY((1U & (~ (IData)(vlSelf->rst))))) {
         Vtop___024root____Vdpiimwrap_top__DOT__u_ysyx_22040088_IFU__DOT__npc_read_TOP(vlSelf->top__DOT__pc_out, __Vtask_top__DOT__u_ysyx_22040088_IFU__DOT__npc_read__3__rdata);
         vlSelf->top__DOT__u_ysyx_22040088_IFU__DOT__inst_data 
             = __Vtask_top__DOT__u_ysyx_22040088_IFU__DOT__npc_read__3__rdata;
+        VL_WRITEF("IF read\n");
     }
     if (vlSelf->top__DOT__mem_mem_ena) {
         Vtop___024root____Vdpiimwrap_top__DOT__u_ysyx_22040088_IFU__DOT__npc_read_TOP(
