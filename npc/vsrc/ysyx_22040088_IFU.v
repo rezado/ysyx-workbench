5c88029d1feef511ac356357a6a947f08e970d69
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-30 17:47:53 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:47:53 up 1 day,  4:43,  4 users,  load average: 0.88, 0.46, 0.20

--- npc/vsrc/ysyx_22040088_IFU.v
+++ npc/vsrc/ysyx_22040088_IFU.v
@@ -34,14 +34,13 @@ assign nextpc = jump_o ? jumppc :
 
 import "DPI-C" function void npc_read(
   input longint raddr, output longint rdata);
-reg [63:0] inst_data;
+wire [63:0] inst_data;
 
 always @(posedge clk) begin
 	if (!rst) begin
 		npc_read(pc, inst_data);
 		$display("IF read");
 	end
-	if (jump_o) inst_data <= 64'b0;
 end
 
 assign inst = (pc[2:0] == 3'b000) ? inst_data[31:0] :
