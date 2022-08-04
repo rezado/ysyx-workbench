456453e458766b7799b047486368b5363c662750
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-04 22:28:58 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:28:58 up  8:50,  7 users,  load average: 0.69, 0.44, 0.40

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -6,7 +6,7 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
-    switch (c->gprs[17]) {
+    switch (c->gpr[17]) {
       case -1: 
         ev.event = EVENT_YIELD;
         c->mepc += 4;
