89777fc69311365bff2400f7acea3788edec24de
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-04 21:17:28 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:17:28 up  7:38,  6 users,  load average: 0.71, 0.85, 0.63

--- abstract-machine/am/src/riscv/npc/ioe.c
+++ abstract-machine/am/src/riscv/npc/ioe.c
@@ -25,7 +25,10 @@ static void fail(void *buf) { panic("access nonexist register"); }
 
 bool ioe_init() {
   for (int i = 0; i < LENGTH(lut); i++)
-    if (!lut[i]) lut[i] = fail;
+    if (!lut[i]) {
+      printf("i:%d fail\n");
+      lut[i] = fail;
+    }
   printf("finish lut check\n");
   __am_timer_init();
   return true;
