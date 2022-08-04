6f9955a5dd17aedb595604c183ccd09ecd8d232f
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-04 22:00:51 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:00:51 up  8:22,  7 users,  load average: 0.28, 0.72, 0.70

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -6,7 +6,7 @@
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
-  printf("into am_irq_handler\n");
+  // printf("into am_irq_handler\n");
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
