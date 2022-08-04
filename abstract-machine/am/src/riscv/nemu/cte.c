37cdab4087ab881c31838cbae13f6723b4f17955
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-04 10:18:58 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:18:58 up  1:40,  6 users,  load average: 0.88, 0.58, 0.28

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,7 +9,10 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case -1: ev.event = EVENT_YIELD; break;
+      case -1: 
+        ev.event = EVENT_YIELD;
+        c->epc += 4;
+        break;
       case 0: case 1: case 2: case 3:
       case 4: case 5: case 6: case 7:
       case 8: case 9: case 10: case 11:
