bb7895ec80468bfc54be43cdf241b8b6e5d68a64
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-05 20:32:18 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:32:18 up 15:57,  8 users,  load average: 1.33, 0.95, 0.73

--- npc/include/isa.h
+++ npc/include/isa.h
@@ -31,11 +31,6 @@ int isa_mmu_check(vaddr_t vaddr, int len, int type);
 #endif
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type);
 
-// interrupt/exception
-vaddr_t isa_raise_intr(word_t NO, vaddr_t epc);
-#define INTR_EMPTY ((word_t)-1)
-word_t isa_query_intr();
-
 // difftest
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc);
 void isa_difftest_attach();
