7f4c5fe74e866ff61164829becf1b52e8a9c5d3a
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-05 19:56:03 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:56:03 up 15:21,  8 users,  load average: 0.59, 0.45, 0.45

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -29,10 +29,10 @@ static void pmem_write(paddr_t addr, int len, word_t data) {
   #endif
 }
 
-// static void out_of_bound(paddr_t addr) {
-//   panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR ") at pc = " FMT_WORD,
-//       addr, CONFIG_MBASE, CONFIG_MBASE + CONFIG_MSIZE, cpu.pc);
-// }
+static void out_of_bound(paddr_t addr) {
+  panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR ") at pc = " FMT_WORD,
+      addr, CONFIG_MBASE, CONFIG_MBASE + CONFIG_MSIZE, cpu.pc);
+}
 
 void init_mem() {
 #if   defined(CONFIG_PMEM_MALLOC)
@@ -53,12 +53,12 @@ void init_mem() {
 word_t paddr_read(paddr_t addr, int len) {
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
-  // out_of_bound(addr);
+  out_of_bound(addr);
   return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
-  // out_of_bound(addr);
+  out_of_bound(addr);
 }
