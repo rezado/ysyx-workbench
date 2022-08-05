987e920979810f2b04bdf8678df5e1250142c6b4
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-05 19:13:20 +0800

    > compile NEMU ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:13:20 up 14:38,  8 users,  load average: 1.65, 0.76, 0.44

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -7,7 +7,7 @@
 int main() {
   // printf("Init NDL\n");
   NDL_Init(0);
-  printf("finish NDL_init\n");
+  // printf("finish NDL_init\n");
   int w, h;
   void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
   assert(bmp);
