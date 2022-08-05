663e3ff3e587cd133ee0e24b372bb2c25270ed03
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-05 21:26:48 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:26:48 up 16:51,  8 users,  load average: 0.31, 0.39, 0.44

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -291,10 +291,7 @@ WB u_WB(
 
 always @(posedge clk) begin
 	if (rst) skip <= 1'b0;
-	else if (wb_inst == 32'b0)
-		skip <= 1'b1;
-	else
-		skip <= 1'b0;
+	else skip <= (wb_inst == 32'b0);
 end
 
 // ebreak
