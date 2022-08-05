8ae06939d27e0ca64e56dab7740b89ccfe86e5d7
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-05 15:57:38 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:57:38 up 13:05,  7 users,  load average: 1.19, 0.72, 0.55

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -53,7 +53,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
 }
 
 void yield() {
-  printf("into yield\n");
+  // printf("into yield\n");
   asm volatile("li a7, -1; ecall");
 }
 
