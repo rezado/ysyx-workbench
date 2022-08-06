7ea37a533c73a7d0b47827b11f3b927ffc1fd8b2
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-06 10:25:48 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:25:48 up 19:27,  8 users,  load average: 0.08, 0.17, 0.23

--- npc/vsrc (copy)/ysyx_22040088_signext.v
+++
@@ -1,9 +0,0 @@
-// 符号扩展
-module ysyx_22040088_signext #(IN_WIDTH = 32, EXT_WIDTH = 64) (
-    input   [IN_WIDTH - 1: 0] in,
-    output  [EXT_WIDTH - 1: 0] out
-);
-
-assign out = {{(EXT_WIDTH - IN_WIDTH){in[IN_WIDTH - 1]}}, in};
-
-endmodule
