25940d9a82a03e7a270005c0ad05e2438fc490a5
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-15 10:29:37 +0800

    > sim RTL ysyx_22040088 吴佳宾 Linux bill-virtual-machine 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:29:37 up 1 day, 21:33,  3 users,  load average: 0.31, 0.25, 0.26

--- npc/csrc/cpu/cpu_exec.cpp
+++ npc/csrc/cpu/cpu_exec.cpp
@@ -60,21 +60,17 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   // DIFFTEST比DUT晚一个周期更新
-  if (instr.val == 0 || top->stall == 1) {
-    // difftest_skip_ref();
-    printf("time:%d pc:%x inst:%x\n", g_nr_guest_inst, _this->pc, instr.val);
-  }
-  else if (!hit) {
-    hit = true;
-  }
-  else if (hit) {
+  if (hit) {
     printf("dnpc:%x inst:%x\n", dnpc, instr.val);
     hit = false;
     IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
   }
+  else if (instr.val == 0 || top->stall == 1) {
+    // difftest_skip_ref();
+    printf("time:%d pc:%x inst:%x\n", g_nr_guest_inst, _this->pc, instr.val);
+  }
   else {
-    // printf("dnpc:%x inst:%x\n", dnpc, instr.val);
-    IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
+    hit = true;
   }
 #ifdef CONFIG_WATCHPOINT
   // scan watchpoints
